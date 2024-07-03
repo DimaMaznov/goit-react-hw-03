062dbd7765e496cf14cd57a54d9fc459ea771fb1
DimaMaznov <maznovdmitriy01@gmail.com>
2024-07-03 15:02:26 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-import { useState} from "react";
+import { useState, useEffect} from "react";
 import ContactList from "./ContactList";
 import SearchBox from "./SearchBox";
 import ContactForm from "./ContactForm";
 
 export default function App() {
     
-    const [values, setValues] = useState([
+  
+
+    const [values, setValues] = useState(() => {
+      const saved = localStorage.getItem("contacts");
+      return JSON.parse(saved).length > 4 ? JSON.parse(saved) : [
         { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
         { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
         { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ]);
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }
+      ];
+    });
+
       const [filter, setFilter] = useState('');
       
       const handleDelete = (id) => {
@@ -24,6 +30,9 @@ export default function App() {
           return [...prevContact, newContact];
         });
       };
+      useEffect(() => {
+        localStorage.setItem("contacts", JSON.stringify(values));
+    }, [values]);
     
     return (
     <div>
