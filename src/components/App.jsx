03728231cf1f40271f4e658050a0780c38b9fdd3
DimaMaznov <maznovdmitriy01@gmail.com>
2024-07-03 15:10:50 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
 
     const [values, setValues] = useState(() => {
       const saved = localStorage.getItem("contacts");
-      return JSON.parse(saved).length > 4 ? JSON.parse(saved) : [
+      return saved && JSON.parse(saved).length > 4 ? JSON.parse(saved) : [
         { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
         { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
         { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
